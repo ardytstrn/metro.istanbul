055132d5d63be353ac77eb85e52f18d73285d752
ardytstrn <yetistirenarda@gmail.com>
2025-05-04 23:33:07 +0300

    feat: add POST method

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import axios from "axios";
 import {
   convertResponse,
@@ -31,6 +32,31 @@ export async function getRequest<T>(
 ): Promise<MetroIstanbulResponse<T>> {
   const rawResponse: MetroIstanbulResponseRaw<T> = (await apiClient.get(path))
     .data;
+  const response: MetroIstanbulResponse<T> = convertResponse(rawResponse);
+
+  if (!response.success) {
+    throw new Error(response.error?.message);
+  } else {
+    return response;
+  }
+}
+
+/**
+ * Sends a POST request to the specified path and returns a
+ * normalized response.
+ *
+ * @template T - The expected type of the data in the response
+ * @param path - The relative path of the API endpoint (e.g. "/GetTimeTable")
+ * @param body - The request body
+ * @throws Will throw an error if the response indicates failure
+ */
+export async function postRequest<T>(
+  path: string,
+  body: any
+): Promise<MetroIstanbulResponse<T>> {
+  const rawResponse: MetroIstanbulResponseRaw<T> = (
+    await apiClient.post(path, body)
+  ).data;
   const response: MetroIstanbulResponse<T> = convertResponse(rawResponse);
 
   if (!response.success) {
